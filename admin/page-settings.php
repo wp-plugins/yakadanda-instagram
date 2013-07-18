be0a3cce7480f07a0164d02677adac5309125990
peterricci <peterricci@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-18 08:38:48 +0000

    added 0.0.31 to tags
    git-svn-id: https://plugins.svn.wordpress.org/yakadanda-instagram/tags/0.0.31@742669 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/page-settings.php
@@ -0,0 +1,102 @@
+<div class="wrap">
+	<div id="icon-edit" class="icon32 icon32-posts-quote"><br></div><h2>Settings</h2>
+  <form action="<?php echo YINSTAGRAM_PLUGIN_URL . '/admin/posteddata-settings.php'; ?>" method="post" enctype="multipart/form-data">
+    <table class="form-table">
+      <tbody>
+        <tr valign="top">
+          <th scope="row">
+            <label for="client_id">Client ID</label>
+          </th>
+          <td>
+            <input type="text" name="client_id" id="client_id" class="regular-text" value="<?php echo $client_id; ?>"/>&nbsp;
+            <?php if ( get_option('yinstagram_access_token') ): ?>
+              <strong style="color: green;" title="<?php echo $data['full_name']; ?>">Connected, logged in as <?php echo $data['username']; ?>.</strong>
+            <?php else: ?>
+              <strong style="color: red;">Not Connected.</strong>
+            <?php endif; ?>
+            <p class="description">Your Instagram Client Id.</p>
+          </td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">
+            <label for="client_secret">Client Secret</label>
+          </th>
+          <td>
+            <input type="text" name="client_secret" id="client_secret" class="regular-text" value="<?php echo $client_secret; ?>"/>
+            <p class="description">Your Instagram Client Secret.</p>
+          </td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Display Your Images</th>
+          <td>
+            <input name="dyi_radio_previous_value" type="hidden" value="<?php echo $data['display_your_images']; ?>"/>
+            <fieldset>
+              <legend class="screen-reader-text">
+                <span>Display Your Images</span>
+              </legend>
+              <label title="Get the most recent media published by a user.">
+                <input type="radio" value="recent" name="display_your_images" <?php echo ( ($data['display_your_images'] == 'recent') || !isset($data['display_your_images']) ) ? 'checked="checked"' : null; ?>>
+                <span>Recent</span>
+              </label>
+              <br>
+              <label title="See the authenticated user's feed.">
+                <input type="radio" value="feed" name="display_your_images" <?php echo ( ($data['display_your_images'] == 'feed') ) ? 'checked="checked"' : null; ?>>
+                <span>Feed</span>
+              </label>
+              <br>
+              <label title="See the authenticated user's list of media they've liked.">
+                <input type="radio" value="liked" name="display_your_images" <?php echo ( ($data['display_your_images'] == 'liked') ) ? 'checked="checked"' : null; ?>>
+                <span>Liked</span>
+              </label>
+              <br>
+              <label title="No">
+                <input type="radio" value="0" name="display_your_images" <?php echo ( ($data['display_your_images'] == '0') && isset($data['display_your_images']) ) ? 'checked="checked"' : null; ?>>
+                <span>No</span>
+              </label>
+            </fieldset>
+          </td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Display The Following Hashtags</th>
+          <td>
+            <fieldset>
+              <legend class="screen-reader-text">
+                <span>Display The Following Hashtags</span>
+              </legend>
+              <label title="Yes">
+                <input type="radio" value="1" name="option_display_the_following_hashtags" <?php echo ($data['option_display_the_following_hashtags'] == '1') ? 'checked="checked"' : null; ?>>
+                <span>Yes</span>
+              </label>
+              <br>
+              <label title="No">
+                <input type="radio" value="0" name="option_display_the_following_hashtags" <?php echo ( ($data['option_display_the_following_hashtags'] == '0') || !isset($data['option_display_the_following_hashtags']) ) ? 'checked="checked"' : null; ?>>
+                <span>No</span>
+              </label>
+              <div id="showHashtags" style="<?php echo ($data['option_display_the_following_hashtags']) ? 'display: block;' : 'display: none;'; ?>">
+                <p>
+                  <textarea id="display_the_following_hashtags" class="large-text code" cols="50" rows="10" name="display_the_following_hashtags"><?php echo ($data['display_the_following_hashtags']) ? $data['display_the_following_hashtags'] : null; ?></textarea>
+                </p>
+                <p class="description">Hashtags separated by comma, e.g. <em style="font-style:normal; color: #464646;">#buildings, #graffiti, #art</em> etc.</p>
+              </div>
+            </fieldset>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p class="submit">
+      <?php if ( get_option('yinstagram_access_token') ): ?>
+        <input id="submit" class="button-primary" type="submit" value="Save Changes" name="submit">&nbsp;
+        <a href="<?php echo YINSTAGRAM_PLUGIN_URL . '/admin/posteddata-settings.php?logout=1'; ?>" class="button-primary">Logout</a>
+      <?php else: ?>
+        <input id="submit" class="button-primary" type="submit" value="Save and Connect" name="submit">
+      <?php endif; ?>
+    </p>
+    <p class="description">
+      <a href="<?php echo YINSTAGRAM_PLUGIN_URL . '/manual.php'; ?>" target="_blank">See these instructions</a> to register Instagram clients.
+    </p>
+    <h3>Shortcode Examples:</h3> 
+    <ul class="sc_examples">
+	    <li>[yinstagram]</li>
+    </ul>
+  </form>
+</div>
---
+++ admin/posteddata-settings.php
@@ -0,0 +1,49 @@
+<?php
+include_once dirname( __FILE__ ) . "/../../../../wp-load.php";
+
+if ($_GET['logout']) {
+  $option = 'yinstagram_access_token';
+  $value = null;
+  update_option( $option, $value );
+} elseif( ($_POST['client_id'] != null) && ($_POST['client_secret'] != null)) {
+  
+  $data = get_option('yinstagram_settings');
+  $token = get_option('yinstagram_access_token');
+  
+  if ( ($_POST['display_your_images'] != '0') && ($_POST['option_display_the_following_hashtags'] == '1') ) {
+    $_POST['option_display_the_following_hashtags'] = '0';
+  }
+  if ( ($_POST['display_your_images'] == '0') && ($_POST['option_display_the_following_hashtags'] == '0') ) {
+    $_POST['display_your_images'] = 'recent';
+  }
+  if ( ($_POST['display_your_images'] == '0') && ($_POST['option_display_the_following_hashtags'] == '1') && empty($_POST['display_the_following_hashtags']) ) {
+    $_POST['display_your_images'] = 'recent';
+    $_POST['option_display_the_following_hashtags'] = '0';
+  }
+  
+  $value = array(
+    'client_id' => $_POST['client_id'],
+    'client_secret' => $_POST['client_secret'],
+    'display_your_images' => $_POST['display_your_images'],
+    'option_display_the_following_hashtags' => $_POST['option_display_the_following_hashtags'],
+    'display_the_following_hashtags' => $_POST['display_the_following_hashtags']
+  );
+  
+  $option = 'yinstagram_settings';
+  update_option( $option, $value );
+  
+  if ( ($data['client_id'] != $_POST['client_id']) || ($data['client_secret'] != $_POST['client_secret']) || !$token ) {
+    // make null the token from database
+    $option = 'yinstagram_access_token';
+    $value = null;
+    update_option( $option, $value );
+    
+    $encodeURIComponent = yinstagram_encodeURIComponent( YINSTAGRAM_PLUGIN_URL . '/authentication.php' );
+    $url = 'https://api.instagram.com/oauth/authorize/?redirect_uri=' . $encodeURIComponent . '&response_type=code&client_id=' . $_POST['client_id'] . '&display=touch';
+    
+    wp_redirect( $url ); exit;
+  }
+  
+}
+
+wp_redirect( admin_url( 'admin.php?page=yinstagram/settings.php' ) ); exit;
